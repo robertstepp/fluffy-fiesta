a0f964db225699123d3684ca1173188ad5e10c48
Robert Stepp <robert@vyperinc.ninja>
2018-05-14 14:42:54 -0700

    Added chosenOuput and menu options

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,24 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.PriorityQueue;
+
+import javax.swing.JOptionPane;
 
 public class Main {
 	private static HashMap<Integer, Double> rainfall = new HashMap<Integer, Double>();
 	private static PriorityQueue pq = new PriorityQueue();
+	private static ArrayList<Integer> dateArray = new ArrayList<Integer>();
+	static String[] menuOptions = { "Output all", "Total rainfall",
+			"Average monthly rainfall", "Month with the most rain",
+			"Month with the least rain" };
+	static String fileName = "";
+	static Object menu = null;
 
+	// Imports the file
+	// https://www.mkyong.com/java/how-to-read-and-parse-csv-file-in-java/
 	public static void importFile(String name) {
 		BufferedReader br = null;
 		String line = "";
@@ -38,9 +49,64 @@ public class Main {
 		}
 	}
 
+	// Populates the priority queue to sort the dates.
+	private static void populatePQ() {
+		Object[] tempArray = rainfall.keySet().toArray(); // Puts the keys from
+															// the hashmap into
+															// a temporary
+															// array.
+		for (int i = 0; i < rainfall.keySet().size(); i++) {
+			pq.add(tempArray[i]);
+		}
+	}
+
+	// Populates the date arraylist with sorted dates from priority queue (Early
+	// to present).
+	private static void populateDA() {
+		int pqSize = pq.size();
+		for (int i = 0; i < pqSize; i++) {
+			dateArray.add((Integer) pq.remove());
+		}
+	}
+
+	// Returns the amount of rainfall for every month.
+	private static void returnRainfallMonthly() {
+		for (int j = 0; j < dateArray.size(); j++) {
+			System.out.print(dateArray.get(j) + "\t");
+			System.out.println(rainfall.get(dateArray.get(j)));
+		}
+	}
+
+	// Gets input from user of the filename and how they want the output.
+	public static void input() {
+		fileName = JOptionPane.showInputDialog(null,
+				"Please enter filename:", "./src/Rainfall.csv");
+		menu = JOptionPane.showInputDialog(null,
+				"Choose output:", "Choose wisely",
+				JOptionPane.QUESTION_MESSAGE, null, menuOptions, null);
+	}
+
+	// Takes the chosen output method and passes it to the associated method.
+	public static void chosenOutput() {
+		if (menu.equals(menuOptions[0])) {
+			returnRainfallMonthly();
+		} else if (menu.equals(menuOptions[1])) {
+			System.out.println(1);
+		} else if (menu.equals(menuOptions[2])) {
+			System.out.println(2);
+		} else if (menu.equals(menuOptions[3])) {
+			System.out.println(3);
+		} else if (menu.equals(menuOptions[4])) {
+			System.out.println(4);
+		}
+	}
+
 	public static void main(String[] args) {
-		String filename = "./src/Rainfall.csv";
-		importFile(filename);
+		input();
+		importFile(fileName);
+		populatePQ();
+		populateDA();
+		chosenOutput();
 
 	}
 
