c40659ac7f7f918f6dc54d7a61cd9bf0aaf9f00e
Robert Stepp <robert@vyperinc.ninja>
2018-05-14 20:49:56 -0700

    Generate Javadoc files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 
 // Java Program by Robert Stepp and Freddie Krueger 05/20/2018
+/**
+ * CS143 Team Project
+ * 3. (Based on Gaddis programming challenges 7.1) Rainfall Class
+ * Create a text file that has 18 doubles with the average monthly rainfall for Olympia in the last year.
+ * Write a RainFall class that stores the total rainfall for each of 18 months into an array of doubles. The program must accept the input out of chronological order and store it in sorted order.
+ * Input dates as an integer, stored in the file as YYYYMM. 
+ * Sample input file:
+ * 201802  1.3
+ * 201701  2.5     
+ * 201803  4.0
+ * 201711  3.5
+ * 201802  1.2
+ *  . . . etc.
+ * The program should have methods that return the following:
+ * • Output the rainfall and date (YYYYMM) in order sorted by date
+ * • The total rainfall on record
+ * • The average monthly rainfall
+ * • The month with the most rain
+ * • The month with the least rain
+ * Demonstrate the class in a complete program. Do not perform input validation.
+ * @author Robert Stepp
+ * @author Freddie Krueger
+ */
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -28,7 +52,12 @@ public class Main {
 	static double monthMinRainfall = 100.0;
 	static double totalMonthlyRainfall = 0.0;
 
-	// Imports the file
+	/**
+	 * Imports the file
+	 * 
+	 * @param name
+	 *            From input user supplied
+	 */
 	// https://www.mkyong.com/java/how-to-read-and-parse-csv-file-in-java/
 	public static void importFile(String name) {
 		BufferedReader br = null;
@@ -61,8 +90,12 @@ public class Main {
 		}
 	}
 
-	// Populates the priority queue to sort the dates.
-	private static void populatePQ() {
+	/**
+	 * Populates the priority queue to sort the dates.
+	 * 
+	 * 
+	 */
+	public static void populatePQ() {
 		Object[] tempArray = rainfall.keySet().toArray(); // Puts the keys from
 		// the hashmap into
 		// a temporary
@@ -72,24 +105,36 @@ public class Main {
 		}
 	}
 
-	// Populates the date arraylist with sorted dates from priority queue (Early
-	// to present).
-	private static void populateDA() {
+	/**
+	 * Populates the date arraylist with sorted dates from priority queue (Early
+	 * to present).
+	 * 
+	 * 
+	 */
+	public static void populateDA() {
 		int pqSize = pq.size();
 		for (int i = 0; i < pqSize; i++) {
 			dateArray.add((Integer) pq.remove());
 		}
 	}
 
-	// Returns the amount of rainfall for every month.
-	private static void returnRainfallMonthly() {
+	/**
+	 * Returns the amount of rainfall for every month.
+	 * 
+	 * 
+	 */
+	public static void returnRainfallMonthly() {
 		for (int j = 0; j < dateArray.size(); j++) {
 			System.out.print(parseMonth(dateArray.get(j)) + "\t");
 			System.out.println(rainfall.get(dateArray.get(j)));
 		}
 	}
 
-	// Gets input from user of the filename and how they want the output.
+	/**
+	 * Gets input from user of the filename and how they want the output.
+	 * 
+	 * 
+	 */
 	public static void input() {
 		fileName = JOptionPane.showInputDialog(null,
 				"Please enter filename:", "./src/Rainfall.csv");
@@ -98,7 +143,11 @@ public class Main {
 				JOptionPane.QUESTION_MESSAGE, null, menuOptions, null);
 	}
 
-	// Takes the chosen output method and passes it to the associated method.
+	/**
+	 * Takes the chosen output method and passes it to the associated method.
+	 * 
+	 * 
+	 */
 	public static void chosenOutput() {
 		if (menu.equals(menuOptions[0])) { // Return output for every month.
 			returnRainfallMonthly();
@@ -136,8 +185,14 @@ public class Main {
 		}
 	}
 
-	// Parses the month digits YYYYMM to the text version of the month.
-	static String parseMonth(Integer date) {
+	/**
+	 * Parses the month digits YYYYMM to the text version of the month.
+	 * 
+	 * @param date
+	 *            from multiple sources
+	 * @return outputDate (String of Month YYYY)
+	 */
+	public static String parseMonth(Integer date) {
 		String month = "";
 		String outputDate = "";
 		char[] dateNumArray = String.valueOf(date).toCharArray();
@@ -149,25 +204,43 @@ public class Main {
 		return outputDate;
 	}
 
-	// Finds the max rainfall and stores it and the date.
-	static void findMax(double rain, Integer date) {
+	/**
+	 * Finds the max rainfall and stores it and the date.
+	 * 
+	 * @param rain
+	 *            from importFile tempArray
+	 * @param date
+	 *            from importFile tempArray
+	 */
+	public static void findMax(double rain, Integer date) {
 		if (rain > monthMaxRainfall) {
 			monthMaxRainfall = rain;
 			monthMax = date;
 		}
 	}
 
-	// Finds the min rainfall and stores it and the date.
-	static void findMin(double rain, Integer date) {
+	/**
+	 * Finds the min rainfall and stores it and the date.
+	 * 
+	 * @param rain
+	 *            from importFile tempArray
+	 * @param date
+	 *            from importFile tempArray
+	 */
+	public static void findMin(double rain, Integer date) {
 		if (rain < monthMinRainfall) {
 			monthMinRainfall = rain;
 			monthMin = date;
 		}
 	}
 
-	// Outputs the month with the least rainfall. Includes an if statement for
-	// no rain.
-	static void leastRain() {
+	/**
+	 * Outputs the month with the least rainfall. Includes an if statement for
+	 * no rain.
+	 * 
+	 * 
+	 */
+	public static void leastRain() {
 		if (monthMinRainfall == 0.0) { // rain.
 			System.out.printf("%s had no measureable rainfall.\n",
 					parseMonth(monthMin));
@@ -176,22 +249,34 @@ public class Main {
 					parseMonth(monthMin), monthMinRainfall);
 	}
 
-	// Outputs the month with the most rainfall.
-	static void mostRain() {
+	/**
+	 * Outputs the month with the most rainfall.
+	 * 
+	 * 
+	 */
+	public static void mostRain() {
 		System.out.printf("%s had the most rainfall at %.2f inches.\n",
 				parseMonth(monthMax), monthMaxRainfall);
 	}
 
-	// Calculates the total rainfall.
-	static void totalRainfall() {
+	/**
+	 * Calculates the total rainfall.
+	 * 
+	 * 
+	 */
+	public static void totalRainfall() {
 		for (int t = 0; t < dateArray.size(); t++) {
 			totalMonthlyRainfall = totalMonthlyRainfall
 					+ rainfall.get(dateArray.get(t));
 		}
 	}
 
-	// Uses the total and dateArray to calculate the average rainfall.
-	static double averageRainfall() {
+	/**
+	 * Uses the total and dateArray to calculate the average rainfall.
+	 * 
+	 * @return Average Monthly Rainfall
+	 */
+	public static double averageRainfall() {
 		double averageMonthlyRainfall = totalMonthlyRainfall / dateArray.size();
 		return averageMonthlyRainfall;
 	}
