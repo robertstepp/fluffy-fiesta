4ca7a243d9206d29a39c4757a420d4589586c5c7
Robert Stepp <robertstepp@users.noreply.github.com>
2018-10-26 18:51:55 -0700

    Create BadClass.java

---
+++ BadClass.java
@@ -0,0 +1,92 @@
+
+public class BadClass {
+
+	protected int x;
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        try {
+            BadClass.testMetrics();
+        } finally {
+            
+        }
+
+    }
+    
+    public static String testMetrics() {
+        int x = 0;
+        if (x < 1) {
+            return "Gibberish";
+        } else if (x ==1 ) {
+            return "I am over one";
+        } else if (x == 2) {
+            return "I am over two";
+        } else if (x == 3) {
+            return "I am over three";
+        } else if (x ==1 ) {
+            return "I am over one";
+        } else if (x == 2) {
+            return "I am over two";
+        } else if (x == 3) {
+            return "I am over three";
+        } else if (x ==1 ) {
+            return "I am over one";
+        } else if (x == 2) {
+            return "I am over two";
+        } else if (x == 3) {
+            return "I am over three";
+        } else if (x ==1 ) {
+            return "I am over one";
+        } else if (x == 2) {
+            return "I am over two";
+        } else if (x == 3) {
+            return "I am over three";
+        } else if (x ==1 ) {
+            return "I am over one";
+        } else if (x == 2) {
+            return "I am over two";
+        } else if (x == 3) {
+            return "I am over three";
+        } else if (x ==1 ) {
+            return "I am over one";
+        } else if (x == 2) {
+            return "I am over two";
+        } else if (x == 3) {
+            return "I am over three";
+        }else if (x ==1 ) {
+            return "I am over one";
+        } else if (x == 2) {
+            return "I am over two";
+        } else if (x == 3) {
+            return "I am over three";
+        } else if (x ==1 ) {
+            return "I am over one";
+        } else if (x == 2) {
+            return "I am over two";
+        } else if (x == 3) {
+            return "I am over three";
+        } else if (x ==1 ) {
+            return "I am over one";
+        } else if (x == 2) {
+            return "I am over two";
+        } else if (x == 3) {
+            return "I am over three";
+        } else {
+            return null;
+        }
+    }
+    
+    @Override
+    public boolean equals(Object theOther) {
+        return false;
+    }
+    
+    @Override
+    public Object clone() {
+        return null;
+    }
+
+}
+
